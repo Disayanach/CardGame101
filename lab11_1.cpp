4f67575493402f9da1052c3a5b6f23cb952d22ed
Disayanach <disayanach.g@gmail.com>
2024-12-20 16:22:37 +0700

    done101

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+    string input;
+    int pressCount = 0;
+    cout << "Press Enter 3 times to reveal your future." << endl;
+    while (pressCount < 3) {
+        getline(cin, input);
+        pressCount++;
+    }
+    srand(time(0));
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int randomIndex = rand() % 9;  
+    cout << "You will get " << grades[randomIndex] << "in this 261102."<<endl;
+    return 0;
+}
